b64cc47b131d0fd309481370dcb21430eb90d699
Jack Rueter <rueter.jack@gmail.com>
2017-01-25 10:11:16 +0000

    After inspection of kom and myv. Removing from svn.

--- testing/gen-paradigms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Shell script to run paradigm generation for a list of words
-# It first cleans a bit, and then calls a perl script that does
-# the processing of the word list and calls make.
-
-perl gen-paradigms.pl item_list.txt
-
--- testing/run_generation.sh
+++
@@ -1,3 +0,0 @@
-
-./gen-paradigms.sh 
-
