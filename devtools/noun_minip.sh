f3e94d163aa7aa43545fa43676cf3fea09af86e4
Trond Trosterud <trond.trosterud@uit.no>
2020-05-25 17:35:32 +0200

    paths

--- devtools/noun_minip.sh
+++ devtools/noun_minip.sh
@@ -25,7 +25,7 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-   echo "${lemma}${form}" | $LOOKUP $GTHOME/langs/kpv/src/generator-gt-norm.xfst
+   echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-kpv/src/generator-gt-norm.xfst
  done
 done
 
